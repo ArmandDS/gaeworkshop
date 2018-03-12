1ca82e4440200a9bc938f469ccf0682c24e338f3
cowbotic <cowbotic@gmail.com>
2018-03-12 17:23:58 +0100

    Update start.py

--- start.py
+++ start.py
@@ -2,16 +2,24 @@ import logging, requests, requests_toolbelt.adapters.appengine
 
 from flask import Flask, render_template, request, jsonify
 from google.appengine.ext import ndb
-from google.appengine.api import memcache, namespace_manager, modules
+from google.appengine.api import memcache, namespace_manager, modules, app_identity
 
 
 application = Flask(__name__)
+main_srv='default'
+project_name=app_identity.app_identity.get_default_version_hostname()
 
 @application.route('/')
 def dashboard():
    requests_toolbelt.adapters.appengine.monkeypatch()
-   stats_1=requests.get('http://1-dot-workshop-23.appspot.com/stats/v1')
-   stats_2=requests.get('http://2-dot-workshop-23.appspot.com/stats/v1')
+   name_v1=modules.modules.get_versions('default')[0]
+   name_v2=modules.modules.get_versions('default')[1]
+   
+   url_stats_1='http://'+name_v1+'-dot-'+main_srv+'-dot-'+project_name+'/stats/v1'
+   url_stats_2='http://'+name_v2+'-dot-'+main_srv+'-dot-'+project_name+'/stats/v1'
+   
+   stats_1=requests.get(url_stats_1)
+   stats_2=requests.get(url_stats_2)
    
    likes1=stats_1.json()['likes']
    views1=stats_1.json()['views']
@@ -23,4 +31,4 @@ def dashboard():
 @application.route('/raw')
 def raw():
   def_srv_versions=modules.modules.get_versions('default')
-  return jsonify({"versions":str(def_srv_versions)})
+  return jsonify({"versions":def_srv_versions})
